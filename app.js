fbafb6490820e86f79bdc847db50fed6df504337
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-20 15:01:58 -0400

    using body parser, get and post request

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  console.log("In another middlewareS!");
-  res.send("<h1>THIS IS / PRODUCT </h1>");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name= "title"><button type="submit">Add product</button></form>'
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
+  //console.log("In another middleware!");
   res.send("<h1>Hello my friend, this is express</h1>");
 });
 
