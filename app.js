a64e232233da65e4e1ca27df8336218ee42ce407
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-21 12:59:01 -0400

    changed name to 404

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(shopRoutes);
 
 //Routing 404 page
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "pagenotfound.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 //server
