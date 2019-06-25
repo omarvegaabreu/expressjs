4e187b6e6f42a15584e516f8f24f0032dc49c6a1
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-25 13:13:48 -0400

    added handlebars

--- app.js
+++ app.js
@@ -9,9 +9,14 @@ const app = express();
 
 //Global configuration values templating engines
 
-//Pug
-app.set("view engine", "pug");
+//Handlebars
+app.engine("handlebars", expressHbs);
+app.set("view engine", "handlebars");
 app.set("views", "views");
+
+//Pug
+// app.set("view engine", "pug");
+// app.set("views", "views");
 
 //Importing routes
 const adminData = require("./routes/admin");
