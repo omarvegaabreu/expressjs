ac111ea3bf1b3ee4bd117f7b00bbf9b4cd40fc18
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-25 14:50:21 -0400

    removing handlebars

--- app.js
+++ app.js
@@ -2,22 +2,12 @@
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
 
 //Node modules functions
 const app = express();
 
 //Global configuration values templating engines
 
-//Handlebars
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs"
-  })
-);
 app.set("view engine", "hbs");
 app.set("views", "views");
 
