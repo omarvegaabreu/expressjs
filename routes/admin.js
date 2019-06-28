49f60f29bd604b03b2412c863a1db5971855d75b
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-28 16:08:58 -0400

    adding mvc file structure

--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,11 @@ const express = require("express");
 
 const path = require("path");
 
+//Importing products js
+const productsController = require("../controllers/products");
+
 //Root directory path to app.js
-const rootDir = require("../util/path");
+//const rootDir = require("../util/path");
 
 //app variables
 const router = express.Router();
@@ -14,15 +17,7 @@ const products = [];
 
 //Route to views folder
 //admin/add-product GET request
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
 //admin/add-product POST request
 
