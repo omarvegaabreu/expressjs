2aa4a308f5701f6474bc377df9b1fc8f13f04a71
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-28 21:15:11 -0400

    created data folder

--- models/product.js
+++ models/product.js
@@ -1,11 +1,12 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    products.push(this);
+    const p = path.join(path.dirname(process.mainModule.filename));
   }
   static fetchAll() {
     return products;
