537e9ecd1b263a6c1d4b1b11711fbd2927d6bee7
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-28 21:29:47 -0400

    not working

--- models/product.js
+++ models/product.js
@@ -6,7 +6,22 @@ module.exports = class Product {
     this.title = t;
   }
   save() {
-    const p = path.join(path.dirname(process.mainModule.filename));
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
   static fetchAll() {
     return products;
